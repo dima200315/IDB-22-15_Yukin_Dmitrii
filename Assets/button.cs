aeab71f7c5cbeb271cdbb85ab812dd9db9945ce3
dima200315 <dima.yukin.2015@gmail.com>
2023-10-02 23:05:31 +0300

    first commit
    1 dz

---
+++ Assets/button.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject CUBE;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void button_click()
+    {
+        if (CUBE.activeSelf)
+        {
+            CUBE.SetActive(false);
+        }
+        else
+        {
+            CUBE.SetActive(true);
+        }
+    }
+    
+}
